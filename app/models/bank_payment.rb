76a30e219b60325c80d7c33d49321c506fe37fe1
Gabriel Prado <gabriel.maturz@gmail.com>
2012-05-30 01:31:09 -0300

    Remove bank payment stubs

--- app/models/bank_payment.rb
+++ app/models/bank_payment.rb
@@ -1,16 +1,17 @@
+# encoding: utf-8
 class BankPayment
-  
+
   attr_accessor :payment_id, :payment_type
-  
+
   def initialize(params)
     @payment_id   = params[:payment_id]
     @payment_type = params[:payment_type]
   end
-  
+
   def make_payment(price)
     account = portal_account_info.account
     agency = portal_account_info.agency
-    
+
     payment_id = case payment_type.to_s
     when "boleto"
       BankPaymentFactory.instance.boleto(account, agency, price)
@@ -22,14 +23,26 @@ class BankPayment
       raise "Unknown payment type"
     end
   end
-  
+
   def status
-    BankPaymentFactory.instance.status(payment_id)
+    self.class.human_statuses[BankPaymentFactory.instance.status(payment_id)]
   end
-  
+
   private
-  
+
   def portal_account_info
     PortalAccountInfo.instance
+  end
+
+  class << self
+
+    def human_statuses
+      {
+        'S05_R01' => 'Pendente',
+        'S05_R02' => 'Efetuado',
+        'S05_E01' => 'Não encontrado'
+      }
+    end
+
   end
 end
--- app/models/bank_payment_factory.rb
+++ app/models/bank_payment_factory.rb
@@ -1,8 +1,7 @@
 class BankPaymentFactory < SoapBase
-  
+
   def boleto(account, agency, price)
     Rails.logger.debug "#{self.class}#boleto"
-    return (rand*1000).to_i #TODO remove this line when wsdl if implemented
     response = client.request :pagar_via_boleto_bancario do
       soap.body = {
         :agencia => agency,
@@ -10,13 +9,12 @@ class BankPaymentFactory < SoapBase
         :valor   => price
       }
     end
-    
+
     response[:pagar_via_boleto_bancario_response][:pagar_via_boleto_bancario_return]
   end
-  
+
   def transferencia(account, agency, price)
     Rails.logger.debug "#{self.class}#transferencia"
-    return (rand*1000).to_i #TODO remove this line when wsdl if implemented
     response = client.request :pagar_via_transferencia_bancaria do
       soap.body = {
         :agencia => agency,
@@ -24,13 +22,12 @@ class BankPaymentFactory < SoapBase
         :valor   => price
       }
     end
-    
+
     response[:pagar_via_transferencia_bancaria_response][:pagar_via_transferencia_bancaria_return]
   end
-  
+
   def deposito(account, agency, price)
     Rails.logger.debug "#{self.class}#deposito"
-    return (rand*1000).to_i #TODO remove this line when wsdl if implemented
     response = client.request :pagar_via_deposito_bancario do
       soap.body = {
         :agencia => agency,
@@ -38,30 +35,29 @@ class BankPaymentFactory < SoapBase
         :valor   => price
       }
     end
-    
+
     response[:pagar_via_deposito_bancario_response][:pagar_via_deposito_bancario_return]
   end
-  
+
   def status(id)
     Rails.logger.debug "#{self.class}#status"
-    return "pendente" #TODO remove this line when wsdl if implemented
     response = client.request :verifica_status_pagamento do
       soap.body = {
         :id_pagamento => id
       }
     end
-    
+
     response[:verifica_status_pagamento_response][:verifica_status_pagamento_return]
   end
-  
+
   class << self
-    
+
     private
-    
+
     def default_wsdl
-      ""
+      "http://localhost:8180/pagamento_C10_G10/services/PagamentoBanco?wsdl"
     end
-    
+
   end
-  
+
 end
