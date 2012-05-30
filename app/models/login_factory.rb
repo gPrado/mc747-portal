bf98d2c462313026df46d9c18698b4bc9ec42da3
Gabriel Prado <gabriel.maturz@gmail.com>
2012-05-30 01:30:32 -0300

    Remove login stub

--- app/models/login_factory.rb
+++ app/models/login_factory.rb
@@ -1,5 +1,5 @@
 class LoginFactory < SoapBase
-  
+
   def login(cpf, passwd)
     Rails.logger.debug "#{self.class}#login"
     response = client.request :authenticate do
@@ -8,19 +8,18 @@ class LoginFactory < SoapBase
         :password => passwd
       }
     end
-      
+
     response[:authenticate_response][:return] != "Invalid user/password"
-    true #TODO remove this line
   end
-    
+
   class << self
-    
+
     private
-    
+
     def default_wsdl
       "http://sci-psych.com/mc747/AuthService/public/index/wsdl"
     end
-  
+
   end
-  
+
 end
